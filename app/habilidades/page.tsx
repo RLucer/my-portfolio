7be2ad0973b09ab50a920901007800d95cb95602
RLucer <lucero.ricardo@gmail.com>
2024-07-11 08:38:18 -0400

    added insig 4

--- app/habilidades/page.tsx
+++ app/habilidades/page.tsx
@@ -12,6 +12,13 @@ export default function HabilidadesPage() {
         <p>Mis habilidades están en constante crecimiento y mejora diaria</p>
 
       </h1>
+
+      <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="d500f85a-e985-45f4-98b3-56738179dd4c" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
+
+      <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="c1750357-f010-43a7-84ae-deb11af02b81" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
+
+      <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="d500f85a-e985-45f4-98b3-56738179dd4c" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
+
       <SkillsComponent />
     </div>
   );
